77b862404dd15c92e9779b2faa4c36b01f6c946d
lordever <lordever@mail.ru>
2025-09-25 20:04:13 +0500

    Setup Tip calculator app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import ComplicatedTab from './component-compound-pattern/bad-practice/bad-tab.component';
-import CompounedComponentBasedTab
-    from './component-compound-pattern/good-practice/compounded-tab.component';
 
 function App() {
     return (
         <>
-            <ComplicatedTab/>
-            <CompounedComponentBasedTab/>
+            <h1 className="text-preset-1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque aliquet nibh.</h1>
+            <h2 className="text-preset-2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit dosa. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. </h2>
+            <h3 className="text-preset-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit dosa. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. </h3>
+            <h4 className="text-preset-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis.</h4>
+            <h5 className="text-preset-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</h5>
+            <h6 className="text-preset-6">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</h6>
         </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,53 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Red Hat Display', 'sans-serif']
+                sans: ['Space   Mono', 'sans-serif']
+            },
+            fontSize: {
+                'preset-1': ['48px', {
+                    lineHeight: '71px',
+                    letterSpacing: '-1px',
+                    fontWeight: '700'
+                }],
+                'preset-2': ['32px', {
+                    lineHeight: "47px",
+                    letterSpacing: '-0.67px',
+                    fontWeight: '700'
+                }],
+                'preset-3': ['24px', {
+                    lineHeight: '36px',
+                    letterSpacing: '0px',
+                    fontWeight: '700'
+                }],
+                'preset-4': ['20px', {
+                    lineHeight: '30px',
+                    letterSpacing: '0px',
+                    fontWeight: '700'
+                }],
+                'preset-5': ['16px', {
+                    lineHeight: '24px',
+                    letterSpacing: '0px',
+                    fontWeight: '700'
+                }],
+                'preset-6': ['13px', {
+                    lineHeight: '19px',
+                    letterSpacing: '0px',
+                    fontWeight: '700'
+                }],
+            },
+            colors: {
+                'gray-600': '#3D6666',
+                'gray-550': '#547878',
+                'gray-500': '#5E7A7D',
+                'gray-400': '#7F9D9F',
+                'gray-300': '#9EBBBD',
+                'gray-200': '#C5E4E7',
+                'gray-50': '#F3F9FA',
+                'green-900': '#00474B',
+                'green-800': '#085C61',
+                'green-400': '#26C2AE',
+                'green-200': '#9FE8DF',
+                'orange-400': '#E17052',
             }
         },
     },
